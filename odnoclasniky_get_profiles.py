b15bff7f40fb819d6c228a8321e04af17791481c
vengeros <vengerhrum@gmail.com>
2022-03-02 22:35:04 +0200

    odnoklasniky spam bot parallel multiple acs

--- odnoclasniky_get_profiles.py
+++ odnoclasniky_get_profiles.py
@@ -1,8 +1,9 @@
-import signal
+import queue
+import threading
 from time import sleep
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -10,10 +11,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 CHROME_DRIVER_PATH = 'chromedriver'
 SVINO_PUBLICS = [
-    'https://ok.ru/pricolypro/members'
+    'https://ok.ru/pricolypro/members',
+    'https://ok.ru/handmade24/members',
+    'https://ok.ru/womenlike/members',
+    'https://ok.ru/ntv/members',
+    'https://ok.ru/semyajornal/members',
 ]
 
-COOKIE_AUTH_CODE = 'YOUR AUTH CODE FROM COOKIE - `AUTHCODE`'
+COOKIE_AUTH_CODE = 'zXNvThL2k_ypv4_djhS6wFdhyGm0SQ3nrBPSzquUldHA6tJ9Rk4ilLgZ_dmT1bcJbCUVN9N0Bl0k2HbOgdsayzCA7C-5eV33ZZ8yZh_cod9wmP7c-uCGkykBU03t1uqEX-At8mU8hchSBYLv_4'
 
 
 def handler(signum, frame):
@@ -21,36 +26,60 @@ def handler(signum, frame):
     raise NoSuchElementException()
 
 
-chrome_options = Options()
-prefs = {"profile.managed_default_content_settings.images": 2}
-chrome_options.add_experimental_option("prefs", prefs)
-browser = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH, chrome_options=chrome_options)
-browser.get('https://ok.ru/')
-browser.add_cookie({'name': 'AUTHCODE', 'value': COOKIE_AUTH_CODE})
-browser.refresh()
-browser.implicitly_wait(5)
+write_queue = queue.Queue()
+publics_queue = queue.Queue()
+for public_link in SVINO_PUBLICS:
+    publics_queue.put(public_link)
 
-signal.signal(signal.SIGALRM, handler)
 
-for public in SVINO_PUBLICS:
-    browser.get(public)
+def task(publics_queue, token):
+    chrome_options = Options()
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    chrome_options.add_experimental_option("prefs", prefs)
+    browser = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH, chrome_options=chrome_options)
+    browser.get('https://ok.ru/')
+    browser.add_cookie({'name': 'AUTHCODE', 'value': token})
+    browser.refresh()
+    browser.implicitly_wait(5)
+
+    public_link = publics_queue.get()
+    browser.get(public_link)
 
     while True:
         try:
             more_button = browser.find_element(By.XPATH, '//*[text()="Показать ещё"]')
-            signal.alarm(7)
+
             while not more_button.is_displayed() or not more_button.is_enabled():
+                # height_before = browser.execute_script("return document.body.scrollHeight")
                 browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                # sleep(0.5)
+                # browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                # height_after = browser.execute_script("return document.body.scrollHeight")
+
+                if len(browser.find_elements(By.XPATH, '//div[@class="ugrid_i"]//a[@class="user-grid-card_img"]')) > 4000:
+                    break
+
                 sleep(0.5)
-            signal.alarm(0)
+
+            browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            sleep(0.3)
             browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             WebDriverWait(browser, 5).until(EC.element_to_be_clickable(more_button))
             more_button.click()
-        except NoSuchElementException:
+        except (NoSuchElementException, TimeoutException):
             break
 
     profiles_elements = browser.find_elements(By.XPATH, '//div[@class="ugrid_i"]//a[@class="user-grid-card_img"]')
-    profile_links = [elem.get_attribute('href') for elem in profiles_elements]
+    profiles_links = [elem.get_attribute('href') for elem in profiles_elements]
 
     with open('odnoclasniky_profiles.txt', mode='a', encoding='utf-8') as myfile:
-        myfile.write('\n'.join(profile_links))
+        myfile.write('\n'.join(profiles_links) + '\n')
+
+    publics_queue.task_done()
+
+
+for i in SVINO_PUBLICS:
+    worker = threading.Thread(target=task, args=(publics_queue, COOKIE_AUTH_CODE,), daemon=True)
+    worker.start()
+
+publics_queue.join()
--- odnoclasniky_spam_profiles.py
+++ odnoclasniky_spam_profiles.py
@@ -4,32 +4,58 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from time import sleep
 import os
+import queue
+import threading
+import random
+
 
 CHROME_DRIVER_PATH = 'chromedriver'
 VIDEO_ABSOLUTE_PATH = os.path.abspath("data/ukrain.mp4")
 
-COOKIE_AUTH_CODE = 'YOUR AUTH CODE FROM COOKIE - `AUTHCODE`'
+TOKENS = [
+    'zXNvThL2k_ypv4_djhS6wFdhyGm0SQ3nrBPSzquUldHA6tJ9Rk4ilLgZ_dmT1bcJbCUVN9N0Bl0k2HbOgdsayzCA7C-5eV33ZZ8yZh_cod9wmP7c-uCGkykBU03t1uqEX-At8mU8hchSBYLv_4',
+    # 'JgbOIuU7mgOwWLlMVQhWxKXLNqNzYoPRuBA1W9wx6y7hIqeecfv61sdy7Zb0qquHSBAwmGADBkocpgb7pZeIPzdKQVzv-qAC5m13LsiTFAMA8NvMxul_eGu7iIZS635optl4M3HZOEArB65j_4',
+    # 'hHaCgGUzvAE30Ve61RgyqlJ81cmqduTgntaFq6cbVBOGdKY6h1_nW2uCi8diBU8oLc5eVNZ35RuPW4DaWViYtjmZfPfujwPyU8_eYjkm4y-jubxWVyAbqdQy4KQLsDyHZB8UUyZo3REy50M6_4',
+]
 
-chrome_options = Options()
-browser = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH, chrome_options=chrome_options)
-browser.get('https://ok.ru/')
-browser.add_cookie({'name': 'AUTHCODE', 'value': COOKIE_AUTH_CODE})
-browser.refresh()
-
-browser.implicitly_wait(3)
-
+profile_links_queue = queue.Queue()
 profile_links = open('odnoclasniky_profiles.txt', 'r').readlines()
+random.shuffle(profile_links)
+
+
 for profile_link in profile_links:
-    browser.get(profile_link)
-    try:
-        write_button = browser.find_element(By.XPATH, '//a[text()="Написать"]')
-        write_button.click()
-        msg_input = browser.find_element(By.XPATH, '//input[@class="attach-file"]')
-        msg_input.send_keys(VIDEO_ABSOLUTE_PATH)
-        browser.find_element(By.XPATH, '//msg-progress[@progress="1"]')# message here
-        msg_input.find_element(By.XPATH, '//msg-button[@title="Отправить"]').click()
-        sleep(1)
-        browser.find_element(By.XPATH, '//*[text()="Вы слишком часто отправляете сообщения разным пользователям. Повторите попытку позже."]')
-        sleep(60 * 5)
-    except (ElementNotInteractableException, NoSuchElementException) as e:
-        continue
+    profile_links_queue.put(profile_link)
+
+
+def task(profile_links_queue, token):
+    chrome_options = Options()
+    browser = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH, chrome_options=chrome_options)
+    browser.get('https://ok.ru/')
+    browser.add_cookie({'name': 'AUTHCODE', 'value': token})
+    browser.refresh()
+    browser.implicitly_wait(3)
+
+    for _ in range(50):
+        profile_link = profile_links_queue.get()
+        browser.get(profile_link)
+        try:
+            write_button = browser.find_element(By.XPATH, '//*[@id="hook_Block_MainMenu"]/div/ul/li[2]/a')
+            write_button.click()
+            msg_input = browser.find_element(By.XPATH, '//input[@class="attach-file"]')
+            msg_input.send_keys(VIDEO_ABSOLUTE_PATH)
+            browser.find_element(By.XPATH, '//msg-progress[@progress="1"]')
+            msg_input.find_element(By.XPATH, '//*[@id="msg_layer"]/msg-app/main/msg-page/div[2]/msg-chat/main/section/footer/msg-posting-form/div/div[1]/div[3]/msg-button[3]').click()
+            sleep(1)
+            browser.find_element(By.XPATH, '//*[text()="Вы слишком часто отправляете сообщения разным пользователям. Повторите попытку позже."]')
+            sleep(60 * 5)
+            # sleep(5)  # todo: remove
+        except (ElementNotInteractableException, NoSuchElementException):
+            pass
+        profile_links_queue.task_done()
+
+
+for token in TOKENS:
+    worker = threading.Thread(target=task, args=(profile_links_queue, token,), daemon=True)
+    worker.start()
+
+profile_links_queue.join()
