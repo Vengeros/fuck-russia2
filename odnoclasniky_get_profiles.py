e7b2a79c10d8f0e83fbe393dab29f8fee2c293a8
vengeros <vengerhrum@gmail.com>
2022-03-05 18:08:24 +0200

    odnoklasniky spam bot

--- odnoclasniky_get_profiles.py
+++ odnoclasniky_get_profiles.py
@@ -1,3 +1,4 @@
+import csv
 import queue
 import threading
 from time import sleep
@@ -16,15 +17,14 @@ SVINO_PUBLICS = [
     'https://ok.ru/womenlike/members',
     'https://ok.ru/ntv/members',
     'https://ok.ru/semyajornal/members',
+    'https://ok.ru/coooking/members',
+    'https://ok.ru/group53082395443390/members'
 ]
 
-COOKIE_AUTH_CODE = 'zXNvThL2k_ypv4_djhS6wFdhyGm0SQ3nrBPSzquUldHA6tJ9Rk4ilLgZ_dmT1bcJbCUVN9N0Bl0k2HbOgdsayzCA7C-5eV33ZZ8yZh_cod9wmP7c-uCGkykBU03t1uqEX-At8mU8hchSBYLv_4'
-
-
-def handler(signum, frame):
-    print('Nothing to load more')
-    raise NoSuchElementException()
-
+ACCOUNTS = [
+    {'AUTHCODE': 'zXNvThL2k_ypv4_djhS6wFdhyGm0SQ3nrBPSzquUldHA6tJ9Rk4ilLgZ_dmT1bcJbCUVN9N0Bl0k2HbOgdsayzCA7C-5eV33ZZ8yZh_cod9wmP7c-uCGkykBU03t1uqEX-At8mU8hchSBYLv_4'},
+    *[{'login': acc[0], 'password': acc[1]} for acc in csv.reader(open('data/order2239902.txt', 'r'), delimiter=':')]
+]
 
 write_queue = queue.Queue()
 publics_queue = queue.Queue()
@@ -32,15 +32,29 @@ for public_link in SVINO_PUBLICS:
     publics_queue.put(public_link)
 
 
-def task(publics_queue, token):
+def task(publics_queue, account):
     chrome_options = Options()
     prefs = {"profile.managed_default_content_settings.images": 2}
     chrome_options.add_experimental_option("prefs", prefs)
     browser = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH, chrome_options=chrome_options)
     browser.get('https://ok.ru/')
-    browser.add_cookie({'name': 'AUTHCODE', 'value': token})
-    browser.refresh()
+
+    if token := account.get('AUTHCODE'):
+        browser.add_cookie({'name': 'AUTHCODE', 'value': token})
+        browser.refresh()
+
+    elif (login := account.get('login')) and (password := account.get('password')):
+        browser.find_element(By.XPATH, '//input[@id="field_email"]').send_keys(login)
+        browser.find_element(By.XPATH, '//input[@id="field_password"]').send_keys(password)
+        browser.find_element(By.XPATH, '//input[@type="submit"]').click()
+
     browser.implicitly_wait(5)
+
+    try:
+        WebDriverWait(browser, 3).until(
+            EC.element_to_be_clickable((By.XPATH, '//input[@id="hook_FormButton_button_accept"]'))).click()
+    except TimeoutException:
+        pass
 
     public_link = publics_queue.get()
     browser.get(public_link)
@@ -48,16 +62,17 @@ def task(publics_queue, token):
     while True:
         try:
             more_button = browser.find_element(By.XPATH, '//*[text()="Показать ещё"]')
+            browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            sleep(0.1)
 
             while not more_button.is_displayed() or not more_button.is_enabled():
-                # height_before = browser.execute_script("return document.body.scrollHeight")
                 browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                # sleep(0.5)
-                # browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                # height_after = browser.execute_script("return document.body.scrollHeight")
+                sleep(0.1)
 
-                if len(browser.find_elements(By.XPATH, '//div[@class="ugrid_i"]//a[@class="user-grid-card_img"]')) > 4000:
+                if len(browser.find_elements(By.XPATH,
+                                             '//div[@class="ugrid_i"]//a[@class="user-grid-card_img"]')) > 4000:
                     break
+                browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
                 sleep(0.5)
 
@@ -72,14 +87,15 @@ def task(publics_queue, token):
     profiles_elements = browser.find_elements(By.XPATH, '//div[@class="ugrid_i"]//a[@class="user-grid-card_img"]')
     profiles_links = [elem.get_attribute('href') for elem in profiles_elements]
 
-    with open('odnoclasniky_profiles.txt', mode='a', encoding='utf-8') as myfile:
+    with open('data/odnoclasniky_profiles.txt', mode='a', encoding='utf-8') as myfile:
         myfile.write('\n'.join(profiles_links) + '\n')
 
+    browser.quit()
     publics_queue.task_done()
 
 
-for i in SVINO_PUBLICS:
-    worker = threading.Thread(target=task, args=(publics_queue, COOKIE_AUTH_CODE,), daemon=True)
+for _, account in zip(SVINO_PUBLICS, ACCOUNTS):
+    worker = threading.Thread(target=task, args=(publics_queue, account,), daemon=True)
     worker.start()
 
 publics_queue.join()
--- odnoclasniky_spam_profiles.py
+++ odnoclasniky_spam_profiles.py
@@ -1,41 +1,66 @@
+import csv
+import os
+import queue
+import threading
+from time import sleep
+import random
+
 from selenium import webdriver
 from selenium.common.exceptions import ElementNotInteractableException, NoSuchElementException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from time import sleep
-import os
-import queue
-import threading
-import random
-
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 CHROME_DRIVER_PATH = 'chromedriver'
 VIDEO_ABSOLUTE_PATH = os.path.abspath("data/ukrain.mp4")
 
-TOKENS = [
-    'zXNvThL2k_ypv4_djhS6wFdhyGm0SQ3nrBPSzquUldHA6tJ9Rk4ilLgZ_dmT1bcJbCUVN9N0Bl0k2HbOgdsayzCA7C-5eV33ZZ8yZh_cod9wmP7c-uCGkykBU03t1uqEX-At8mU8hchSBYLv_4',
-    # 'JgbOIuU7mgOwWLlMVQhWxKXLNqNzYoPRuBA1W9wx6y7hIqeecfv61sdy7Zb0qquHSBAwmGADBkocpgb7pZeIPzdKQVzv-qAC5m13LsiTFAMA8NvMxul_eGu7iIZS635optl4M3HZOEArB65j_4',
-    # 'hHaCgGUzvAE30Ve61RgyqlJ81cmqduTgntaFq6cbVBOGdKY6h1_nW2uCi8diBU8oLc5eVNZ35RuPW4DaWViYtjmZfPfujwPyU8_eYjkm4y-jubxWVyAbqdQy4KQLsDyHZB8UUyZo3REy50M6_4',
+ACC_CHUNK_SIZE = 7
+ACCOUNTS = [
+    # {'AUTHCODE': 'zXNvThL2k_ypv4_djhS6wFdhyGm0SQ3nrBPSzquUldHA6tJ9Rk4ilLgZ_dmT1bcJbCUVN9N0Bl0k2HbOgdsayzCA7C-5eV33ZZ8yZh_cod9wmP7c-uCGkykBU03t1uqEX-At8mU8hchSBYLv_4'},
+    *[{'login': acc[0], 'password': acc[1]} for acc in csv.reader(open('data/order2239902.txt', 'r'), delimiter=':')]
 ]
 
+
+def chunks(lst, n):
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
 profile_links_queue = queue.Queue()
-profile_links = open('odnoclasniky_profiles.txt', 'r').readlines()
-random.shuffle(profile_links)
+if not os.path.exists('data/odnoclasniky_profiles.csv'):
+    profile_links = open('data/odnoclasniky_profiles.txt', 'r').readlines()
+    with open('data/odnoclasniky_profiles.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['account', 'proceeded'])
+        profile_links = list(set(profile_links))
+        data = [[profile_link.strip('\n'), 0] for profile_link in profile_links]
+        writer.writerows(data)
+
+profiles = list(csv.DictReader(open('data/odnoclasniky_profiles.csv', 'r', encoding='UTF8')))
+random.shuffle(profiles)
+
+for profile in profiles:
+    profile_links_queue.put(profile['account'])
 
 
-for profile_link in profile_links:
-    profile_links_queue.put(profile_link)
-
-
-def task(profile_links_queue, token):
+def task(profile_links_queue, account):
     chrome_options = Options()
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    chrome_options.add_experimental_option("prefs", prefs)
     browser = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH, chrome_options=chrome_options)
     browser.get('https://ok.ru/')
-    browser.add_cookie({'name': 'AUTHCODE', 'value': token})
-    browser.refresh()
     browser.implicitly_wait(3)
 
-    for _ in range(50):
+    if token := account.get('AUTHCODE'):
+        browser.add_cookie({'name': 'AUTHCODE', 'value': token})
+        browser.refresh()
+    elif (login := account.get('login')) and (password := account.get('password')):
+        browser.find_element(By.XPATH, '//input[@id="field_email"]').send_keys(login)
+        browser.find_element(By.XPATH, '//input[@id="field_password"]').send_keys(password)
+        browser.find_element(By.XPATH, '//input[@type="submit"]').click()
+
+    for _ in range(10):
         profile_link = profile_links_queue.get()
         browser.get(profile_link)
         try:
@@ -43,19 +68,27 @@ def task(profile_links_queue, token):
             write_button.click()
             msg_input = browser.find_element(By.XPATH, '//input[@class="attach-file"]')
             msg_input.send_keys(VIDEO_ABSOLUTE_PATH)
-            browser.find_element(By.XPATH, '//msg-progress[@progress="1"]')
+            video_progress = browser.find_element(By.XPATH, '//msg-progress[@progress="1"]')
+            WebDriverWait(browser, 10).until(EC.element_to_be_clickable(video_progress))
             msg_input.find_element(By.XPATH, '//*[@id="msg_layer"]/msg-app/main/msg-page/div[2]/msg-chat/main/section/footer/msg-posting-form/div/div[1]/div[3]/msg-button[3]').click()
             sleep(1)
             browser.find_element(By.XPATH, '//*[text()="Вы слишком часто отправляете сообщения разным пользователям. Повторите попытку позже."]')
-            sleep(60 * 5)
-            # sleep(5)  # todo: remove
+            break
         except (ElementNotInteractableException, NoSuchElementException):
             pass
         profile_links_queue.task_done()
 
 
-for token in TOKENS:
-    worker = threading.Thread(target=task, args=(profile_links_queue, token,), daemon=True)
-    worker.start()
+for account_chunk in chunks(ACCOUNTS, ACC_CHUNK_SIZE):
+    workers = []
+    for account in account_chunk:
+        worker = threading.Thread(target=task, args=(profile_links_queue, account,), daemon=True)
+        workers.append(worker)
+
+    for worker in workers:
+        worker.start()
+
+    for worker in workers:
+        worker.join()
 
 profile_links_queue.join()
